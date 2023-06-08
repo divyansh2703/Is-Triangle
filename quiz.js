a5e6daffe9d7ec58b0af91afd1e3b8b9958e1c8a
divyansh <doshdivyansh1@gmail.com>
2023-06-08 23:35:22 +0530

    almost finished product

---
+++ quiz.js
@@ -0,0 +1,20 @@
+const quizForm = document.querySelector('.quiz-form');
+const submitAnswerBtn = document.querySelector('submit-answer-btn');
+const outputEl = document.querySelector('#output');
+
+const correctAnswer = ["90°","Right angled"];
+
+function calculateScore(){
+    let score = 0;
+    let index = 0;
+    const fromResult = new FormData(quizForm);
+    for(let value of fromResult.values()){
+        if(value === correctAnswer[index]){
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    console.log(score);
+}
+
+submitAnswerBtn.addEventListener('click',calculateScore);
